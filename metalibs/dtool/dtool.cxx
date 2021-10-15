95b3c9a9f403321d4f9875bee0c384f845225ebc
Brian Lach <brianlach72@gmail.com>
2021-10-14 20:24:10 -0400

    export a symbol for stub libdtool.dll

--- metalibs/dtool/dtool.cxx
+++ metalibs/dtool/dtool.cxx
@@ -8,3 +8,11 @@
 // to compile when making libdtool.so in NO_DEFER mode, which generates an
 // empty library that itself links with all the other shared libraries that
 // make up libdtool.
+
+// This is the one file in this directory which is actually compiled.  It
+// exists just so we can have some symbols and make the compiler happy.
+
+#if defined(_WIN32) && !defined(CPPPARSER) && !defined(LINK_ALL_STATIC)
+__declspec(dllexport)
+#endif
+int dtool;
